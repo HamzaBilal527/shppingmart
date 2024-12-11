539dcc6faaa9d2d1de7682d5d7cbfa0fe99875f9
HamzaBilal527 <hamzabilal21@gmail.com>
2024-12-12 01:56:49 +0500

    Checking

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,10 @@ function App() {
     dispatch(checkAuth());
   }, [dispatch]);
 
-  if (isLoading) return <Skeleton className="w-[800] bg-black h-[600px]" />;
+  if (isLoading)
+    return (
+      <Skeleton className="w-[800] bg-black h-[600px]" />
+    );
 
   console.log(isLoading, user);
 
@@ -45,51 +48,86 @@ function App() {
           element={
             <CheckAuth
               isAuthenticated={isAuthenticated}
-              user={user}
-            ></CheckAuth>
+              user={user}></CheckAuth>
           }
         />
         <Route
           path="/auth"
           element={
-            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            <CheckAuth
+              isAuthenticated={isAuthenticated}
+              user={user}>
               <AuthLayout />
             </CheckAuth>
-          }
-        >
+          }>
           <Route path="login" element={<AuthLogin />} />
-          <Route path="register" element={<AuthRegister />} />
+          <Route
+            path="register"
+            element={<AuthRegister />}
+          />
         </Route>
         <Route
           path="/admin"
           element={
-            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            <CheckAuth
+              isAuthenticated={isAuthenticated}
+              user={user}>
               <AdminLayout />
             </CheckAuth>
-          }
-        >
-          <Route path="dashboard" element={<AdminDashboard />} />
-          <Route path="products" element={<AdminProducts />} />
+          }>
+          <Route
+            path="dashboard"
+            element={<AdminDashboard />}
+          />
+          <Route
+            path="products"
+            element={<AdminProducts />}
+          />
           <Route path="orders" element={<AdminOrders />} />
-          <Route path="features" element={<AdminFeatures />} />
+          <Route
+            path="features"
+            element={<AdminFeatures />}
+          />
         </Route>
         <Route
           path="/shop"
           element={
-            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            <CheckAuth
+              isAuthenticated={isAuthenticated}
+              user={user}>
               <ShoppingLayout />
             </CheckAuth>
-          }
-        >
+          }>
           <Route path="home" element={<ShoppingHome />} />
-          <Route path="listing" element={<ShoppingListing />} />
-          <Route path="checkout" element={<ShoppingCheckout />} />
-          <Route path="account" element={<ShoppingAccount />} />
-          <Route path="paypal-return" element={<PaypalReturnPage />} />
-          <Route path="payment-success" element={<PaymentSuccessPage />} />
-          <Route path="search" element={<SearchProducts />} />
+          <Route
+            path="listing"
+            element={<ShoppingListing />}
+          />
+          <Route
+            path="checkout"
+            element={<ShoppingCheckout />}
+          />
+          <Route
+            path="account"
+            element={<ShoppingAccount />}
+          />
+          <Route
+            path="paypal-return"
+            element={<PaypalReturnPage />}
+          />
+          <Route
+            path="payment-success"
+            element={<PaymentSuccessPage />}
+          />
+          <Route
+            path="search"
+            element={<SearchProducts />}
+          />
         </Route>
-        <Route path="/unauth-page" element={<UnauthPage />} />
+        <Route
+          path="/unauth-page"
+          element={<UnauthPage />}
+        />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
